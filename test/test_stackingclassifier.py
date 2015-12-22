5ea3dbb552f924a5958bc72b1735a0360e154020
Necati Demir <ndemir@demir.web.tr>
2015-12-22 21:40:17 +0200

    modified testing script according to the changes

--- test/test_stackingclassifier.py
+++ test/test_stackingclassifier.py
@@ -3,7 +3,7 @@ __author__="Necati Demir <ndemir@demir.web.tr>"
 
 import numpy as np
 from StackingClassifier import StackingClassifier
-from sklearn import linear_model
+from sklearn import linear_model, metrics
 from sklearn import tree
 import random
 
@@ -11,14 +11,36 @@ import random
 X = np.random.random((100, 10))
 y = np.array([random.sample([0, 1],  1)[0]  for i in range(100)]).ravel()
 
+#use ONEGO technique to create stacking model
 stacking_classifier = StackingClassifier(
     base_classifiers=[
-        linear_model.SGDClassifier(loss='log'),
-        linear_model.LogisticRegression(),
-        tree.DecisionTreeClassifier()
+        linear_model.SGDClassifier(loss='log', random_state=0),
+        linear_model.LogisticRegression(random_state=0),
+        tree.DecisionTreeClassifier(random_state=0)
     ],
-    combiner=linear_model.LogisticRegression()
+    combiner=linear_model.LogisticRegression(),
+    technique=StackingClassifier.ONEGO
 )
 
 stacking_classifier.fit(X, y)
 predicted_y_proba = stacking_classifier.predict_proba(X)
+print metrics.roc_auc_score(y, predicted_y_proba)
+# Since the dataset is meaningless, roc_auc_score will not produce meaningful result
+# I am using it just to see, if there are any problems
+
+#use OUTOFFOLDS technique to create stacking model
+stacking_classifier = StackingClassifier(
+    base_classifiers=[
+        linear_model.SGDClassifier(loss='log', random_state=0),
+        linear_model.LogisticRegression(random_state=0),
+        tree.DecisionTreeClassifier(random_state=0)
+    ],
+    combiner=linear_model.LogisticRegression(),
+    technique=StackingClassifier.OUTOFFOLDS
+)
+
+stacking_classifier.fit(X, y)
+predicted_y_proba = stacking_classifier.predict_proba(X)
+print metrics.roc_auc_score(y, predicted_y_proba)
+# Since the dataset is meaningless, roc_auc_score will not produce meaningful result
+# I am using it just to see, if there are any problems
